88acac403278c22b0580c2c571f00a65cfece1fb
Luca Tobón <96082778+Lucarte@users.noreply.github.com>
2022-01-06 10:39:42 +0100

    add short contryList

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let countryList = [ 
+    {name: 'Colombia', capital: 'Bogotá', population: 50882884, borders: ['BRA', 'ECU', 'PAN', 'PER', 'VEN'], flag: 'https://flagcdn.com/co.svg' },
+    {name: 'United States of America', capital: 'Washington, D.C.', population: 329484123, borders: ['CAN', 'MEX'], flag: 'https://flagcdn.com/us.svg' },
+    {name: 'Germany', capital: 'Berlin', population: 83240525, borders: ['AUT', 'BEL', 'CZE', 'DNK', 'FRA', 'LUX', 'NLD', 'POL', 'CHE'], flag: 'https://flagcdn.com/us.svg' },
+]
