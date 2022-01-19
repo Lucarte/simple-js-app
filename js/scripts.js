8666a6ea00bcd2b96c211b2c17c8edc485a0d972
Luca Tobón <96082778+Lucarte@users.noreply.github.com>
2022-01-19 20:46:51 +0100

    add eventListener and addListItem function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,68 @@
-let countryList = [ 
-    {name: 'Colombia', capital: 'Bogotá', population: 50882884, borders: ['BRA', 'ECU', 'PAN', 'PER', 'VEN'], flag: 'https://flagcdn.com/co.svg' },
-    {name: 'United States of America', capital: 'Washington, D.C.', population: 329484123, borders: ['CAN', 'MEX'], flag: 'https://flagcdn.com/us.svg' },
-    {name: 'Germany', capital: 'Berlin', population: 83240525, borders: ['AUT', 'BEL', 'CZE', 'DNK', 'FRA', 'LUX', 'NLD', 'POL', 'CHE'], flag: 'https://flagcdn.com/us.svg' }
-];
+// Now write the code within IIFE
+let countryRepository = (function () {
+    let repository = [
+        {name: 'Colombia', capital: 'Bogotá', population: 50882884, borders: ['BRA', 'ECU', 'PAN', 'PER', 'VEN'], flag: 'https://flagcdn.com/co.svg' },
+        {name: 'United States of America', capital: 'Washington, D.C.', population: 329484123, borders: ['CAN', 'MEX'], flag: 'https://flagcdn.com/us.svg' },
+        {name: 'Germany', capital: 'Berlin', population: 83240525, borders: ['AUT', 'BEL', 'CZE', 'DNK', 'FRA', 'LUX', 'NLD', 'POL', 'CHE'], flag: 'https://flagcdn.com/us.svg' }
+    ];
 
+    function add (country) {
+        if (
+            typeof country === 'object' &&
+            'capital' in country &&
+            'borders' in country &&
+            'population' in country
+        ) {
+            repository.push (country);  
+        } else {
+        console.log ('this is not a country');    
+        }
+    }
+    function getAll () {
+        return repository;
+    }
+    
+    function addListItem (country) {
+        let countryList = document.querySelector ('.country-list');
+        let listCountry = document.createElement ('li');
+        let button = document.createElement ('button');
+        button.innerText = country.name;
+        button.classList.add ('button-class');
+        listCountry.appendChild (button);
+        countryList.appendChild (listCountry);
+        button.addEventListener ('click', function () {
+            console.log(country);
+        })
+    }
+
+    function showDetails (country) {
+        console.log (country);
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+
+    };
+})(); 
+
+console.log(countryRepository.getAll())
 
 // This would be the regualr for Loop
-/* for (let i = 0; i < countryList.length; i++){
+/* for (let i = 0; i < repository.length; i++){
     
-    if (countryList[i].population > 99999999){
+    if (repository[i].population > 99999999){
         
-        document.write('<p>' + countryList[i].name + ' ' + '(population: ' + countryList[i].population + ') ' + ' - WOW! A very populated country! ');
+        document.write('<p>' + repository[i].name + ' ' + '(population: ' + repository[i].population + ') ' + ' - WOW! A very populated country! ');
     } else {
-        document.write('<p>' + countryList[i].name + ' ' + '(population: ' + countryList[i].population + ') ');
+        document.write('<p>' + repository[i].name + ' ' + '(population: ' + repository[i].population + ') ');
     }
 } */
 
 // This would be the external forEach function 
-/* countryList.forEach(nombreDeLaFuncion);
+/* repository.forEach(nombreDeLaFuncion);
 
 function nombreDeLaFuncion(item) {
     console.log(item);
@@ -25,13 +70,19 @@ function nombreDeLaFuncion(item) {
  */
 
 // This would be the internal forEach function
-/* countryList.forEach(function(item){
+/* repository.forEach(function(item){
     console.log(item);
 }) */
 
 // This would be the forEach arrow function
-/* countryList.forEach( country => document.write('<p>' + country.name + ' ' + '(population: ' + country.population + ') ')); */
+/* repository.forEach( country => document.write('<p>' + country.name + ' ' + '(population: ' + country.population + ') ')); */
 
-countryList.forEach( country => {
-    return document.write( `<p> ${country.name} Population: (${country.population}) </p>`)
+// Using template literals:
+/* repository.forEach( country => document.write(`<p> ${country.name} (population: ${country.population}`) )); */
+
+
+countryRepository.getAll().forEach(function(country) {
+    countryRepository.addListItem(country);
 });
+
+
